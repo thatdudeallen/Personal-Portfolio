a6ff849fd320c625c0db35fc3e338e33638cc396
Tatenda Allen Chisvo <161702427+thatdudeallen@users.noreply.github.com>
2026-05-07 13:55:35 +0200

    Add files via upload

---
+++ Personal Portfolio/Untitled-1.js
@@ -0,0 +1,18 @@
+<script>
+
+var tablinks = documents.getElementsByClassName("tab-links");
+var tabcontents = documents.getElementsByClassName("tab-contents");
+
+function opentab(tabname)
+{
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+</script>
